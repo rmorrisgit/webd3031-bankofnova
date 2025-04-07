1d52922620322ece1dfef77802c07a83c6f03fc2
rmorrisgit <ryanmorris971@gmail.com>
2025-04-07 17:18:58 -0300

    fix(build): fixed build errors

--- src/app/(UserLayout)/(User_Pages)/success/page.tsx
+++ src/app/(UserLayout)/(User_Pages)/success/page.tsx
@@ -6,11 +6,12 @@ import DashboardCard from '../../components/shared/DashboardCard';
 const SamplePage = () => {
   return (
     <PageContainer title="Sample Page" description="this is Sample page">
-      <DashboardCard title="Success">
-      </DashboardCard>
+<DashboardCard title="Success">
+  <div>Transfer successful!</div>
+</DashboardCard>
+
     </PageContainer>
   );
 };
 
 export default SamplePage;
-
